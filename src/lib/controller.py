37b7eccc5fb0d50e5d170b9baf4d45410854bd4a
Kirti Purohit <kirti.purohit@quantum.com>
2025-03-30 15:01:14 +0530

    Portfolio parameters

--- src/lib/controller.py
+++ src/lib/controller.py
@@ -3,6 +3,8 @@ import json
 from src.lib.get_tradebook import generate_adjusted_tradebook, load_tradebook
 from src.lib.get_stock_info import get_stock_info_store, get_index_data
 from src.lib.generate_holdings import generate_holdings_from_tradebook
+from src.lib.portfolio_parameters import portfolio_parameters
+from src.models.portfolio import Portfolio
 
 class Controller:
     def __init__(self):
@@ -18,7 +20,7 @@ class Controller:
         self.adjusted_tradebook = generate_adjusted_tradebook(self.tradebook, self.stock_info_store)
         self.index_returns = get_index_data()
         self.holdings = generate_holdings_from_tradebook(self.symbols, self.adjusted_tradebook, self.index_returns, self.stock_info_store)
-
+        self.portfolio = portfolio_parameters(self.holdings, self.stock_info_store)
         # Separate holdings into current and past holdings
         self.current_holdings = [holding for holding in self.holdings if holding.quantity != 0]
         self.past_holdings = [holding for holding in self.holdings if len(holding.realized_profit_history) != 0]
